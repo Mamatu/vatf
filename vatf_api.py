fcb70a58f10bd725df1c82b7713520a0569a6c9d
Marcin Matula <marcin.matula@gmail.com>
2022-04-28 21:18:23 +0200

    Registered shell api

--- vatf_api.py
+++ vatf_api.py
@@ -6,6 +6,9 @@ from vatf.executor import player as exec_player
 from vatf.generator import sleep as gen_sleep
 from vatf.executor import sleep as exec_sleep
 
+from vatf.generator import shell as gen_shell
+from vatf.executor import shell as exec_shell
+
 class API_TYPE(Enum):
     EXECUTOR = 1,
     GENERATOR = 2
@@ -13,8 +16,9 @@ class API_TYPE(Enum):
 _apiType = API_TYPE.GENERATOR
 _player_api = {API_TYPE.GENERATOR : gen_player, API_TYPE.EXECUTOR : exec_player}
 _sleep_api = {API_TYPE.GENERATOR : gen_sleep, API_TYPE.EXECUTOR : exec_sleep}
+_shell_api = {API_TYPE.GENERATOR : gen_shell, API_TYPE.EXECUTOR : exec_shell}
 
-_modules = {"player" : _player_api, "sleep" : _sleep_api}
+_modules = {"player" : _player_api, "sleep" : _sleep_api, "shell" : _shell_api}
 
 def set_api_type(apiType):
     global _apiType
