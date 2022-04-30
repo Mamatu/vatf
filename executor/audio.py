c820d636c3e2321fb62b35795d79d6ad17676ecf
Marcin Matula <marcin.matula@gmail.com>
2022-04-30 10:14:22 +0200

    audio.py - changed papy invocation

--- executor/audio.py
+++ executor/audio.py
@@ -4,6 +4,6 @@ from vatf.vatf_register import public_api
 @public_api("audio")
 def record_inputs_outputs():
     global _rec_bg
-    mkdir.mkdir_with_counter("./recordings/session")
-    rec_command = f"python vatf_utils/papy.py --recorder=gst --rec --dir $(cat {filepath})"
+    output_path = mkdir.mkdir_with_counter("./recordings/session")
+    rec_command = f"python vatf_utils/papy.py --recorder=gst --rec --dir {output_path}"
     shell.bg(rec_command)
