0303ed441bb9ee75295ce8db83c6096cffc6cffb
Marcin Matula <marcin.matula@gmail.com>
2022-05-01 00:32:03 +0200

    shell.py - kills all children

--- executor/shell.py
+++ executor/shell.py
@@ -1,6 +1,7 @@
 from vatf.utils import utils
 from vatf.vatf_register import public_api
 import os
+import psutil
 import subprocess
 
 import atexit
@@ -16,6 +17,10 @@ def bg(command):
     #command = command.split(" ")
     process = subprocess.Popen(command, shell=True)
     def on_exit(process):
+        parent = psutil.Process(process.pid)
+        children = parent.children(recursive=True)
+        for child in children:
+            child.kill()
         process.terminate()
         process.wait()
     atexit.register(on_exit, process)
