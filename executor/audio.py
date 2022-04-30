da47b54224e74c996e09752147946af8ec74ec24
Marcin Matula <marcin.matula@gmail.com>
2022-04-30 17:43:46 +0200

    audio.py - record_inputs_outputs - logging

--- executor/audio.py
+++ executor/audio.py
@@ -1,9 +1,12 @@
 from vatf.executor import shell, mkdir
 from vatf.vatf_register import public_api
 
+import logging
+
 @public_api("audio")
 def record_inputs_outputs():
     global _rec_bg
     output_path = mkdir.mkdir_with_counter("./recordings/session")
     rec_command = f"python vatf_utils/papy.py --recorder=gst --rec --dir {output_path}"
+    logging.debug(f"{record_inputs_outputs.__name__}: {rec_command}")
     shell.bg(rec_command)
