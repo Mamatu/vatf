74a9e138fce3127d1998e97d0b266e39b18e362c
Marcin Matula <marcin.matula@gmail.com>
2022-04-30 17:51:51 +0200

    audio.py - proper path to papy

--- executor/audio.py
+++ executor/audio.py
@@ -7,6 +7,6 @@ import logging
 def record_inputs_outputs():
     global _rec_bg
     output_path = mkdir.mkdir_with_counter("./recordings/session")
-    rec_command = f"python vatf_utils/papy.py --recorder=gst --rec --dir {output_path}"
+    rec_command = f"python3 vatf/utils/papy.py --recorder=gst --rec --dir {output_path}"
     logging.debug(f"{record_inputs_outputs.__name__}: {rec_command}")
     shell.bg(rec_command)
