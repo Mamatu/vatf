92ecb517bfa5e70ec551e7cb91c00fa134da0cbb
Marcin Matula <marcin.matula@gmail.com>
2022-04-30 22:44:47 +0200

    executor/shell.py - terminate bg process on finish of script

--- executor/shell.py
+++ executor/shell.py
@@ -3,6 +3,10 @@ from vatf.vatf_register import public_api
 import os
 import subprocess
 
+import atexit
+
+_popens = []
+
 @public_api("shell")
 def fg(command):
     os.system(command)
@@ -10,4 +14,8 @@ def fg(command):
 @public_api("shell")
 def bg(command):
     command = command.split(" ")
-    subprocess.Popen(command)
+    process = subprocess.Popen(command)
+    def on_exit(process):
+        process.terminate()
+        process.wait()
+    atexit.register(on_exit, process)
