4b38529e483eb31d6173719229d3f632c3187a8e
Marcin Matula <marcin.matula@gmail.com>
2022-05-01 16:51:33 +0200

    shell.py - enables logging

--- executor/shell.py
+++ executor/shell.py
@@ -16,13 +16,13 @@ def fg(command):
 @public_api("shell")
 def bg(command):
     process = subprocess.Popen(command, shell=True)
-    #logging.debug(f"Run process {process.pid} in background")
+    logging.debug(f"Run process {process.pid} in background")
     def on_exit(process):
         parent = psutil.Process(process.pid)
         children = parent.children(recursive=True)
         for child in children:
             child.kill()
         process.terminate()
-        #logging.debug(f"Killed and terminated process {process.pid} with children")
+        logging.debug(f"Killed and terminated process {process.pid} with children")
         process.wait()
     atexit.register(on_exit, process)
