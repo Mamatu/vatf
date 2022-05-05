b2f985706da934b09106835679d7ab15747761a7
Marcin Matula <marcin.matula@gmail.com>
2022-05-05 20:49:55 +0000

    date_regex and date_format moved to config - init commit

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -74,6 +74,8 @@ class VaLog:
         va_log = data["va_log"]
         self.path = va_log["path"]
         self.timedelta = datetime.timedelta(**va_log["timedelta"])
+        self.date_regex = va_log["date_regex"]
+        self.date_format = va_log["date_format"]
 
 class Config:
     def __init__(self, config_json_path = None, schema_json_path = None):
@@ -114,6 +116,10 @@ class Config:
         return []
     def get_log_path(self):
         return self.va_log.path
+    def get_date_regex(self):
+        return self.va_log.date_regex
+    def get_date_format(self):
+        return self.va_log.date_format
 
 class ConfigProxy:
     def __init__(self, config_json_path = None, schema_json_path = None):
@@ -147,6 +153,14 @@ class ConfigProxy:
         if not self.config:
             return ""
         return self.config.get_log_path()
+    def get_date_regex(self):
+        if not self.config:
+            raise Exception("config is not loaded")
+        return self.va_log.date_regex
+    def get_date_format(self):
+        if not self.config:
+            raise Exception("config is not loaded")
+        return self.va_log.date_format
 
 def _abs_path_to_schema():
     import pathlib
