d093366bc73ad97b412120f9da0e15978f4eeb49
Marcin Matula <marcin.matula@gmail.com>
2022-05-19 17:36:19 +0000

    log_snapshot - work continuation

--- executor/log_snapshot.py
+++ executor/log_snapshot.py
@@ -17,54 +17,87 @@ from watchdog.events import FileSystemEventHandler
 
 from threading import Timer, Lock
 
-#from https://stackoverflow.com/a/48741004
-class _RepeatTimer(Timer):
-    def run(self):
-        while not self.finished.wait(self.interval):
-            self.function(*self.args, **self.kwargs)
-
-class _MonitorHandler(FileSystemEventHandler):
-    def __init__(self, restart_timeout, log_path):
-        self.restart_timeout = restart_timeout
-        self.timepoint = None
-        self.log_path = log_path
-    def on_modified(self, event):
-        if event.is_directory: return
-        src_path = event.src_path
-        if src_path == self.log_path:
-            if not self.timepoint:
-                global _mutex
-                _mutex.acquire()
-                try:
-                    global _timepoint
-                    _timepoint = time.time()
-                finally:
-                    _mutex.release()
-
-_logger_thread = None
-class _Ctx:
-    def __init__(self, now, log_path, config_path, output_path, snapshot_path):
-        self.now = now
-        self.log_path = log_path
-        self.config_path = config_path
-        self.snapshot_path = snapshot_path
-        self.output_path = output_path
-
 _ctx = None
 _observer = None
 _timer = None
 _timepoint = None
-_mutex = None
+_mutex = Lock()
+_restart_timeout = None
+_restart_command = None
+_log_path = None
+_shell_cmd = None
+_shell_cmd_process = None
+
+def _lock(func):
+    def wrapper():
+        global _mutex
+        _mutex.acquire()
+        try:
+            return func()
+        finally:
+            _muter.release()
+    return wrapper
+
+def _setup_command(shell_cmd, shell_cmd_rtimeout, log_path):
+    global _log_path, _restart_timeout, _shell_cmd
+    _restart_timeout = shell_cmd_rtimeout
+    _log_path = log_path
+    _shell_cmd = shell_cmd
+
+def _start_command():
+    global _shell_cmd, _shell_cmd_process, _restart_command
+    def restart():
+        global _shell_cmd_process, _shell_cmd
+        if _shell_cmd_proces: shell.kill(_shell_cmd_process)
+        _shell_cmd_process = shell.bg(_shell_cmd)
+    _restart_command = restart
+    restart()
+
+def _start_timer():
+    #from https://stackoverflow.com/a/48741004
+    class _RepeatTimer(Timer):
+        def run(self):
+            while not self.finished.wait(self.interval):
+                self.function(*self.args, **self.kwargs)
+    @_lock
+    def timepoint_observer():
+        global _timepoint, _restart_command, _restart_timeout
+        _current_timepoint = time.time()
+        if abs(_current_timepoint - _timepoint) > _restart_timeout:
+            _restart_command()
+
 
 def _start_observer(log_path, restart_timeout):
     global _observer, _timer
+    class _MonitorHandler(FileSystemEventHandler):
+        def __init__(self, log_path, on_log_modified):
+            self.log_path = log_path
+            self.on_log_modified = on_log_modified
+        def on_modified(self, event):
+            if event.is_directory: return
+            src_path = event.src_path
+            if src_path == self.log_path:
+                self.on_log_modified()
     observer = Observer()
-    monitor_handler = _MonitorHandler(restart_timeout)
-    def restart_callback():
-        global _mutex
-    _timer = _RepeatTimer(0.5, )
+    @_lock
+    def update_timepoint():
+        global _timepoint
+        _timepoint = time.time()
+    monitor_handler = _MonitorHandler(_log_path, update_timepoint)
     observer.schedule(monitor_handler, path = log_path, recursive = False)
+    observer.start()
     _observer = observer
+
+@public_api("log_snapshot")
+def start(log_path, shell_cmd, restart_timeout):
+    if shell_cmd:
+        shell_cmd = shell_cmd.format(session_path = session_path)
+        _setup_command(shell_cmd, shell_cmd_rtimeout, log_path)
+        _start_command()
+        _start_timer()
+        _start_observer(log_path, shell_cmd_rtimeout)
+    else:
+        raise Exception("Only variant with shell_cmd is currently supported")
 
 @public_api("log_snapshot")
 def start_from_config():
@@ -78,11 +111,7 @@ def start_from_config():
     session_path = mkdir.mkdir_with_counter("./logs/session")
     log_path = command.get_log_path()
     log_path = log_path.format(session_path = session_path)
-    if shell_cmd:
-        shell_cmd = shell_cmd.format(session_path = session_path)
-        _start_observer(log_path, shell_cmd_rtimeout)
-    if callable(now): now = now()
-    _ctx = _Ctx(now = now, snapshot_path = snapshot_path, config_path = config_path, log_path = log_path, output_path = output_path)
+    start(log_path, shell_cmd, shell_cmd_rtimeout)
 
 def stop():
     global _ctx
--- executor/tests/log_snapshot.py
+++ executor/tests/log_snapshot.py
@@ -30,79 +30,24 @@ class LogSnapshotTests(TestCase):
                     f.writelines(data)
         return path
     def test_log_with_timestamps(self):
-        text = [
-        "2022-01-29 20:54:55.567 line1\n",
-        "2022-01-29 20:54:55.567 line2\n",
-        "2022-01-29 20:54:55.568 line3\n",
-        "2022-01-29 20:54:55.569 line4\n",
-        "2022-01-29 20:54:55.570 line5\n",
-        "2022-01-29 20:54:55.600 line6\n",
-        "2022-01-29 20:54:56.568 line7\n"
-        ]
-        with patch.object(sys, 'argv', ['', '', 'executor/tests/data/config.json']):
-            path1 = self.create_file("w", text)
-            path2 = utils.get_temp_filepath()
-            DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
-            now = datetime.datetime.strptime("2022-01-29 20:54:55.569", DATE_FORMAT)
-            log_snapshot.start(log_path = path1, snapshot_path = path2, now = now)
-            log_snapshot.stop()
-            with open(path2, "r") as f:
-                text = f.readlines()
-                logging.debug(text)
-                self.assertEqual(4, len(text))
-                self.assertEqual(text[0].rstrip(), "2022-01-29 20:54:55.569 line4")
-                self.assertEqual(text[1].rstrip(), "2022-01-29 20:54:55.570 line5")
-                self.assertEqual(text[2].rstrip(), "2022-01-29 20:54:55.600 line6")
-                self.assertEqual(text[3].rstrip(), "2022-01-29 20:54:56.568 line7")
-            os.remove(path1)
-            os.remove(path2)
-    def test_log_without_timestamps(self):
-        text = [
-        "2022-01-29 20:54:55.567 line1\n",
-        "line1\n",
-        "2022-01-29 20:54:55.567 line2\n",
-        "line2\n",
-        "2022-01-29 20:54:55.568 line3\n",
-        "line3\n",
-        "2022-01-29 20:54:55.569 line4\n",
-        "line4\n",
-        "2022-01-29 20:54:55.570 line5\n",
-        "line5\n",
-        "2022-01-29 20:54:55.600 line6\n",
-        "line6\n",
-        "2022-01-29 20:54:56.568 line7\n"
-        "line7\n",
-        ]
-        with patch.object(sys, 'argv', ['', '', 'executor/tests/data/config.json']):
-            path1 = self.create_file("w", text)
-            path2 = utils.get_temp_filepath()
-            DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
-            now = datetime.datetime.strptime("2022-01-29 20:54:55.569", DATE_FORMAT)
-            log_snapshot.start(path1, path2, now = now)
-            log_snapshot.stop()
-            with open(path2, "r") as f:
-                text = f.readlines()
-                logging.debug(text)
-                self.assertEqual(7, len(text))
-                #self.assertEqual(8, len(text))
-                self.assertEqual(text[0].rstrip(), "2022-01-29 20:54:55.569 line4"),
-                self.assertEqual(text[1].rstrip(), "line4"),
-                self.assertEqual(text[2].rstrip(), "2022-01-29 20:54:55.570 line5"),
-                self.assertEqual(text[3].rstrip(), "line5")
-                self.assertEqual(text[4].rstrip(), "2022-01-29 20:54:55.600 line6")
-                self.assertEqual(text[5].rstrip(), "line6")
-                self.assertEqual(text[6].rstrip(), "2022-01-29 20:54:56.568 line7")
-                #self.assertEqual(text[7].rstrip(), "line7")
-            os.remove(path1)
-            os.remove(path2)
-    def test_log_(self):
-        with patch.object(sys, 'argv', ['', '', 'executor/tests/data/config.json']):
-            path2 = utils.get_temp_filepath()
-            DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f"
-            now = datetime.datetime.strptime("2022-02-03 17:32:34.090", DATE_FORMAT)
-            log_snapshot.start(log_path = "executor/tests/data/test.log", snapshot_path = path2, now = now)
-            section = log_snapshot.stop()
-            with open(path2, "r") as f:
-                text = f.readlines()
-                logging.debug(text)
-                self.assertEqual(4, len(text))
+        log_path = utils.get_temp_filepath()
+        log_file = open(log_path, "a")
+        utils.touch(log_path)
+        def log_generator():
+            nonlocal log_file
+            counter = 0
+            while counter <= 40:
+                log_line = f"line{counter + 1}"
+                now = datetime.datetime.now()
+                line = f"{now} {log_line}\n"
+                log_file.write(line)
+                log_file.flush()
+                logging.debug(f"Write {line} into {log_path}")
+                time.sleep(0.1)
+                counter = counter + 1
+        t = threading.Thread(target = log_generator)
+        convert_to_log_zone.side_effect = lambda dt: dt #ToDo: To overload config loading, it should be removed in the future
+        t.start()
+        log_snapshot.start()
+        t.join()
+        log_file.close()
