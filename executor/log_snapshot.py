c6862503ac6f9d7c5d25daab49efd33b35d893de
Marcin Matula <marcin.matula@gmail.com>
2022-05-21 00:02:39 +0200

    log_snaphost.py - refactoring

--- executor/log_snapshot.py
+++ executor/log_snapshot.py
@@ -16,68 +16,7 @@ from vatf.utils.utils import lock
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-from threading import Timer, Lock
-
-_ctx = None
-_observer = None
-_timer = None
-_timepoint = None
-_mutex = Lock()
-_restart_timeout = None
-_restart_command = None
-_log_path = None
-_shell_cmd = None
-_shell_cmd_process = None
-
-def _setup_command(shell_cmd, restart_timeout, log_path):
-    global _log_path, _restart_timeout, _shell_cmd
-    _restart_timeout = restart_timeout
-    _log_path = log_path
-    _shell_cmd = shell_cmd
-
-def _start_command():
-    global _shell_cmd, _shell_cmd_process, _restart_command
-    def restart():
-        global _shell_cmd_process, _shell_cmd
-        if _shell_cmd_process: shell.kill(_shell_cmd_process)
-        _shell_cmd_process = shell.bg(_shell_cmd)
-    _restart_command = restart
-    restart()
-
-def _start_timer():
-    #from https://stackoverflow.com/a/48741004
-    class _RepeatTimer(Timer):
-        def run(self):
-            while not self.finished.wait(self.interval):
-                self.function(*self.args, **self.kwargs)
-    @lock(_mutex)
-    def timepoint_observer():
-        global _timepoint, _restart_command, _restart_timeout
-        _current_timepoint = time.time()
-        if _timepoint and abs(_current_timepoint - _timepoint) * 1000 > _restart_timeout:
-            _restart_command()
-        _timepoint = _current_timepoint
-
-def _start_observer(log_path, restart_timeout):
-    global _observer, _timer
-    class _MonitorHandler(FileSystemEventHandler):
-        def __init__(self, log_path, on_log_modified):
-            self.log_path = log_path
-            self.on_log_modified = on_log_modified
-        def on_modified(self, event):
-            if event.is_directory: return
-            src_path = event.src_path
-            if src_path == self.log_path:
-                self.on_log_modified()
-    observer = Observer()
-    @lock(_mutex)
-    def update_timepoint():
-        global _timepoint
-        _timepoint = time.time()
-    monitor_handler = _MonitorHandler(_log_path, update_timepoint)
-    observer.schedule(monitor_handler, path = log_path, recursive = False)
-    observer.start()
-    _observer = observer
+from threading import Timer, RLock
 
 @public_api("log_snapshot")
 def start(log_path, shell_cmd, restart_timeout):
@@ -104,6 +43,86 @@ def start_from_config():
     shell_cmd = shell_cmd.format(session_path = session_path)
     start(log_path, shell_cmd, restart_timeout)
 
-def stop():
+_ctx = None
+_observer = None
+_timer = None
+_timepoint = None
+_mutex = RLock()
+_restart_timeout = None
+_restart_command = None
+_log_path = None
+_shell_cmd = None
+_shell_cmd_process = None
+
+def _setup_command(shell_cmd, restart_timeout, log_path):
+    global _log_path, _restart_timeout, _shell_cmd
+    _restart_timeout = restart_timeout
+    _log_path = log_path
+    _shell_cmd = shell_cmd
+
+def _start_command():
+    global _shell_cmd, _shell_cmd_process, _restart_command
+    @lock(_mutex)
+    def restart():
+        global _shell_cmd_process, _shell_cmd
+        if _shell_cmd_process: shell.kill(_shell_cmd_process)
+        _shell_cmd_process = shell.bg(_shell_cmd)
+    _restart_command = restart
+    restart()
+
+def _stop_command():
+    global _shell_cmd_process
     if _shell_cmd_process:
         shell.kill(_shell_cmd_process)
+        _shell_cmd_process = None
+
+def _start_timer():
+    #from https://stackoverflow.com/a/48741004
+    class _RepeatTimer(Timer):
+        def run(self):
+            while not self.finished.wait(self.interval):
+                self.function(*self.args, **self.kwargs)
+    @lock(_mutex)
+    def timepoint_observer():
+        global _timepoint, _restart_command, _restart_timeout
+        _current_timepoint = time.time()
+        if _timepoint and abs(_current_timepoint - _timepoint) * 1000 > _restart_timeout:
+            _restart_command()
+        _timepoint = _current_timepoint
+
+def _start_observer(log_path, restart_timeout):
+    global _observer, _timer
+    class _MonitorHandler(FileSystemEventHandler):
+        def __init__(self, log_path, on_log_modified):
+            self.log_path = log_path
+            self.on_log_modified = on_log_modified
+        def on_modified(self, event):
+            if event.is_directory: return
+            if event.src_path == self.log_path:
+                #if self.on_log_modified == None: import pdb; pdb.set_trace()
+                self.on_log_modified()
+    observer = Observer()
+    @lock(_mutex)
+    def update_timepoint():
+        global _timepoint
+        _timepoint = time.time()
+    monitor_handler = _MonitorHandler(_log_path, update_timepoint)
+    _observer = observer
+    observer.schedule(monitor_handler, path = log_path, recursive = False)
+    observer.start()
+
+@lock(_mutex)
+def _stop_observer():
+    global _observer
+    if _observer:
+        _observer.stop()
+        _observer.join()
+        _observer = None
+
+@lock(_mutex)
+def stop():
+    _stop_observer()
+    _stop_command()
+
+import atexit
+atexit.register(stop)
--- executor/tests/log_snapshot.py
+++ executor/tests/log_snapshot.py
@@ -16,20 +16,28 @@ from vatf.executor import log_snapshot
 from vatf.utils import utils, config
 
 _counter = None
-def _log_generator(filepath, lines):
-    global _counter
+_generated_lines = []
+
+def _reset():
+    global _counter, _generated_lines
+    _counter = None
+    _generated_lines = []
+
+def _log_generator(filepath, lines_count):
+    global _counter, _generated_lines
     if not _counter:
         _counter = 0
-    while _counter <= lines:
+    while _counter < lines_count:
         log_line = f"line{_counter + 1}"
         now = datetime.datetime.now()
         line = f"{now} {log_line}\n"
+        _generated_lines.append(line)
         log_file = open(filepath, "a")
         log_file.write(line)
         log_file.flush()
         log_file.close()
-        time.sleep(0.0001)
         _counter = _counter + 1
+        time.sleep(0.0001)
 
 def _log_generator_thread(filepath, lines):
     t = threading.Thread(target = _log_generator, args = (filepath,lines,))
@@ -38,11 +46,12 @@ def _log_generator_thread(filepath, lines):
 
 class LogSnapshotTests(TestCase):
     def __init__(self, arg):
-        logging.basicConfig(level=logging.DEBUG)
         TestCase.__init__(self, arg)
     def setUp(self):
         from vatf.utils import config
+        logging.getLogger().setLevel(logging.INFO)
         config._reset()
+        _reset()
     def create_file(self, mode, data = None):
         path = utils.get_temp_filepath()
         with open(path, mode) as f:
@@ -53,12 +62,18 @@ class LogSnapshotTests(TestCase):
                     f.writelines(data)
         return path
     def test_log_with_timestamps(self):
+        global _generated_lines
         log_path = utils.get_temp_filepath()
         log_path_1 = utils.get_temp_filepath()
-        t = _log_generator_thread(log_path, 2253)
+        lines_count = 2253
+        t = _log_generator_thread(log_path, lines_count)
         utils.touch(log_path_1)
-        log_snapshot.start(log_path_1, f"while true; do bash -c \"cat {log_path} >> {log_path_1}; sync\"; sleep 0.01; done", 10)
+        log_snapshot.start(log_path_1, f"while true; do bash -c \"cat {log_path} > {log_path_1}; sync\"; done", 10)
         time.sleep(1)
-        with open(log_path_1, "r") as f:
-            self.assertTrue(2253, len(f.readlines()))
         t.join()
+        with open(log_path_1, "r") as f:
+            rlines = f.readlines()
+            self.assertEqual(lines_count, len(rlines))
+            self.assertEqual(lines_count, len(_generated_lines))
+            for idx in range(len(rlines)):
+                self.assertEqual(rlines[idx], _generated_lines[idx])
