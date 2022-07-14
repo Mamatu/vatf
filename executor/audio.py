a0e9b760d577e007c6cf7006d44d37576c8849be
Marcin Matula <marcin.matula@gmail.com>
2022-07-14 14:23:07 +0000

    audio - print info on called function

--- executor/audio.py
+++ executor/audio.py
@@ -1,11 +1,12 @@
 from vatf.api import shell, mkdir
 
 from vatf import vatf_api
-from vatf.utils import config_handler
+from vatf.utils import config_handler, utils
 import logging
 
 @vatf_api.public_api("audio")
 def record_inputs_outputs():
+    logging.info(f"{record_inputs_outputs.__name__}: {utils.name_and_args()}")
     output_path = mkdir.mkdir_with_counter("./recordings/session")
     rec_command = f"python3 vatf/utils/papy.py --recorder=gst --rec --dir {output_path}"
     logging.debug(f"{record_inputs_outputs.__name__}: {rec_command}")
@@ -13,6 +14,7 @@ def record_inputs_outputs():
 
 @vatf_api.public_api("audio")
 def record_inputs_outputs_from_config(**kwargs):
+    logging.info(f"{record_inputs_outputs_from_config.__name__}: {utils.name_and_args()}")
     audio_dir_path_key = "audio.path"
     output = config_handler.handle([audio_dir_path_key], **kwargs)
     output_path = output[audio_dir_path_key]
