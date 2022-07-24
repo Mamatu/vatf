dae16801cef5ccb6b7b046372bf2d50a6fecc8b0
Marcin Matula <marcin.matula@gmail.com>
2022-07-24 21:39:31 +0200

    multiple configs processing - bugfixing and tests

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -34,10 +34,54 @@ class _Config:
     def get(self, arg, raiseIfNotFound = True):
         return get_attr(self.data, arg, raiseIfNotFound = raiseIfNotFound)
 
+def _iterate_dict(dict_key, callback):
+    def _make_key_chain(key_chain, key):
+        new_key_chain = key_chain.copy()
+        new_key_chain.append(key)
+        return new_key_chain
+    def iterate_dict(dict_key, callback, key_chain):
+        if isinstance(dict_key, dict):
+            for k,v in dict_key.items():
+                dict_key[k] = iterate_dict(v, callback, key_chain = _make_key_chain(key_chain, k))
+            dict_key1 = callback(key_chain, dict_key)
+            return (dict_key1 if dict_key1 is not None else dict_key)
+        if isinstance(dict_key, list):
+            for x in range(len(dict_key)):
+                dict_key[x] = iterate_dict(dict_key[x], callback, key_chain = _make_key_chain(key_chain, dict_key))
+        return dict_key
+    return iterate_dict(dict_key, callback, key_chain = [])
+
+def _check(dict1, dict2):
+    keys1 = dict1.keys()
+    keys2 = dict2.keys()
+    for k in keys2:
+        if k in keys1:
+            if (isinstance(dict1[k], dict) ^ isinstance(dict2[k], dict)):
+                raise Exception(f"Both values of the same key must be dict or another type")
+            if not (isinstance(dict1[k], dict) and isinstance(dict2[k], dict)):
+                raise Exception(f"{dict1} and {dict2} contains the same keys!")
+
+def _update_dict_deeply(obj, item):
+    def callback_1(key1, dict1):
+        def callback_2(key2, dict2):
+            if key1 == key2 and (key1 is not None or key2 is not None):
+                _check(dict1, dict2)
+                temp_dict = dict1.copy()
+                dict1.update(dict2)
+                dict2.update(dict1)
+            return dict2
+        _iterate_dict(item, callback_2)
+        return dict1
+    return _iterate_dict(obj, callback_1)
+
+def _convert_if_dict(obj):
+    def callback(key, obj):
+        return SimpleNamespace(**obj)
+    return _iterate_dict(obj, callback)
+
 def load_raw(config_json_pathes, schema_json_path = _abs_path_to_schema()):
     if isinstance(config_json_pathes, str):
         config_json_pathes = [config_json_pathes]
-    data = None
     array = []
     for config_json_path in config_json_pathes:
         with open(config_json_path) as f:
@@ -48,13 +92,11 @@ def load_raw(config_json_pathes, schema_json_path = _abs_path_to_schema()):
                     logging.debug(f"Validation {config_json_path} by use schema {schema.name}")
                     jsonschema.validate(data, schema = json.load(schema))
         with open(config_json_path) as f:
-            data = json.load(f, object_hook = lambda d: SimpleNamespace(**d))
-            array.append(data)
+            array.append(json.load(f))
+    data = {}
     for item in array:
-        if data is None:
-            data = item.__dict__
-        else:
-            data.__dict__.update(item.__dict__)
+        data = _update_dict_deeply(data, item)
+    data = _convert_if_dict(data)
     return _Config(data)
 
 def load_default_format(custom_format):
@@ -89,7 +131,7 @@ def load(config_json_pathes, custom_format = {}, schema_json_path = _abs_path_to
         return _dict
     format_dict = {}
     if hasattr(c.data, "format") and c.data.format:
-        format_dict = make_dict(c.data.format)
+        format_dict.update(c.data.format.__dict__)
     if custom_format:
         format_dict.update(custom_format)
     process_format(c.data, format_dict)
