73c874e30b26319c1a04ab425185336d9f931f1a
Marcin Matula <marcin.matula@gmail.com>
2022-12-29 16:08:01 +0100

    utils - grep moved to separeted module

--- utils/utils.py
+++ utils/utils.py
@@ -77,6 +77,14 @@ def parse_number_suffix(string):
     logging.debug(f"{parse_number_suffix.__name__}: {output}")
     return output
 
+def grep(filepath, regex, removeTmpFiles = True, maxCount = -1, fromLine = 1, onlyMatch = False):
+    from vatf.utils import grep
+    return grep.grep(filepath, regex, removeTmpFiles, maxCount, fromLine, onlyMatch)
+
+def grep_regex_in_line(filepath, grep_regex, match_regex, removeTmpFiles = True, maxCount = -1, fromLine = 1):
+    from vatf.utils import grep
+    return grep.grep_regex_in_line(filepath, grep_regex, match_regex, removeTmpFiles, maxCount, fromLine)
+
 def get_counter(dirpath, pattern, suffix = None):
     def remove_suffix(m, suffix):
         if suffix != None:
@@ -94,95 +102,6 @@ def get_counter(dirpath, pattern, suffix = None):
     return i
 
 import enum
-
-class GrepEntry:
-    def __init__(self, line_number = None, matched = None, line_offset = 0):
-        if line_number:
-            try:
-                self.line_number = int(line_number.rstrip())
-                self.line_number = self.line_number + line_offset
-            except Exception as ex:
-                logging.error(f"{line_number} cannot be converted to int! ${ex}")
-                raise ex
-        self.matched = matched.rstrip()
-    def __getitem__(self, idx):
-        if idx == 0:
-            return self.line_number
-        if idx == 1:
-            return self.matched
-        raise IndexError
-    def search_in_matched(self, rec):
-        self.matched = rec.search(self.matched)
-        return self.matched
-    def __str__(self):
-        return f"({self.line_number}, {self.matched})"
-    @staticmethod
-    def FromSplit(line, line_offset = 0):
-        out = line.split(':', 1)
-        return GrepEntry(out[0], out[1], line_offset)
-
-def grep(filepath, regex, removeTmpFiles = True, maxCount = -1, fromLine = 1, onlyMatch = False):
-    if fromLine < 1:
-        raise Exception(f"Invalid fromLine value {fromLine}")
-    if maxCount < -1:
-        raise Exception(f"Invalid value of maxCount {maxCount}. It should be > -1")
-    lineNumber = True #hardcode
-    def makeArgs(lineNumber, maxCount):
-        o_arg = " -o" if onlyMatch else ""
-        n_arg = " -n" if lineNumber else ""
-        m_arg = " -m {maxCount}" if maxCount > -1 else ""
-        return f"{o_arg}{n_arg}{m_arg} -a"
-    args = makeArgs(lineNumber, maxCount)
-    command = f"grep {args} \"{regex}\""
-    if fromLine > 1:
-        command = f"sed -n '{fromLine},$p' {filepath} | {command}"
-    else:
-        command = f"{command} {filepath}"
-    if command == None:
-        raise Exception("Grep command was failed on initialization")
-    logging.debug(f"{grep.__name__}: {command}")
-    with open_temp_file() as fout, open_temp_file() as ferr:
-        logging.debug(f"{grep.__name__}: fout {fout.name}")
-        logging.debug(f"{grep.__name__}: ferr {ferr.name}")
-        def readlines(f):
-            lines = f.readlines()
-            line_offset = 0 if fromLine < 1 else fromLine - 1
-            if not lineNumber:
-                lines = [GrepEntry(matched = l, line_offset = line_offset) for l in lines]
-            else:
-                lines = [GrepEntry.FromSplit(l, line_offset = line_offset) for l in lines]
-            return lines
-        process = subprocess.Popen(command, shell=True, stdout=fout, stderr=ferr)
-        process.wait()
-        if os.path.getsize(ferr.name) > 0:
-            ferr.seek(0)
-            err = readlines(ferr)
-            remove()
-            raise Exception(err)
-        fout.seek(0)
-        out = readlines(fout)
-        return out
-
-def grep_regex_in_line(filepath, grep_regex, match_regex, removeTmpFiles = True, maxCount = -1, fromLine = 1):
-    """
-    :filepath - filepath for greping
-    :grep_regex - regex using to match line by grep
-    :match_regex - regex to extract specific data from line
-    :removeTmpFiles - remove tmp files when finished
-    :maxCount - max count of matched, if it is -1 it will be infinity
-    :fromLine - start searching from specific line
-    """
-    if fromLine < 1:
-        raise Exception(f"Invalid fromLine value {fromLine}")
-    logging.debug(f"{grep_regex_in_line.__name__}: {name_and_args()}")
-    out = grep(filepath, grep_regex, removeTmpFiles, maxCount = maxCount, fromLine = fromLine)
-    rec = re.compile(match_regex)
-    matched_lines = []
-    for o in out:
-        matched = o.search_in_matched(rec)
-        if matched:
-            matched_lines.append(o)
-    return matched_lines
 
 def get_modification_date(filename):
     t = os.path.getmtime(filename)
