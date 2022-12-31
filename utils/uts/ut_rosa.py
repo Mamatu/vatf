5917999859a008b9cd177791f477e1eef92c1b90
Marcin Matula <marcin.matula@gmail.com>
2022-12-31 16:54:38 +0100

    ut_rosa - tests are runnable

--- utils/uts/ut_rosa.py
+++ utils/uts/ut_rosa.py
@@ -17,9 +17,6 @@ import random
 from vatf.utils import rosa
 
 class TestRosa:
-    def __init__(self, arg):
-        logging.basicConfig(level=logging.DEBUG)
-        TestCase.__init__(self, arg)
     @staticmethod
     def _get_temp_filename():
         import tempfile
@@ -33,11 +30,11 @@ class TestRosa:
         loaded = rosa.LoadSamplesSampleRate(file1)
         loaded_sample_rate = loaded[1]
         loaded_samples = loaded[0]
-        self.assertNotEqual(None, loaded_sample_rate)
-        self.assertEqual(16000, loaded_sample_rate)
+        assert(None != loaded_sample_rate)
+        assert (16000 == loaded_sample_rate)
         samples = numpy.array(samples)
         is_equal = numpy.array_equal(samples, loaded_samples)
-        self.assertTrue(is_equal)
+        assert (is_equal)
         rosa.PrintSamplesSampleRate(loaded)
         os.remove(file1)
     def test_waveform_npz_save(self):
@@ -51,11 +48,11 @@ class TestRosa:
         data = numpy.load(file1, allow_pickle=False)
         loaded_sample_rate = data["sample_rate"]
         loaded_samples = data["samples"]
-        self.assertNotEqual(None, loaded_sample_rate)
-        self.assertEqual(16000, loaded_sample_rate)
+        assert(None != loaded_sample_rate)
+        assert (16000 == loaded_sample_rate)
         samples = numpy.array(samples)
         is_equal = numpy.array_equal(samples, loaded_samples)
-        self.assertTrue(is_equal)
+        assert(is_equal)
         os.remove(file1)
     def test_calculate_means(self):
         sample_rate = 16000
@@ -65,11 +62,11 @@ class TestRosa:
         samples = samples + [3] * sample_rate
         samples = numpy.array(samples)
         means, sample_rate = rosa.CalculateMeans((samples, sample_rate), normalize = False)
-        self.assertEqual(3, len(means))
-        self.assertEqual(1, sample_rate)
-        self.assertEqual(1, means[0])
-        self.assertEqual(2, means[1])
-        self.assertEqual(3, means[2])
+        assert(3 == len(means))
+        assert(1 == sample_rate)
+        assert(1 == means[0])
+        assert(2 == means[1])
+        assert(3 == means[2])
     def test_calculate_means_with_normalization(self):
         sample_rate = 16000
         samples = []
@@ -79,12 +76,12 @@ class TestRosa:
         samples = samples + [4] * sample_rate
         samples = numpy.array(samples)
         means, sample_rate = rosa.CalculateMeans((samples, sample_rate), normalize = True)
-        self.assertEqual(4, len(means))
-        self.assertEqual(1, sample_rate)
-        self.assertEqual(0.25, means[0])
-        self.assertEqual(.5, means[1])
-        self.assertEqual(.75, means[2])
-        self.assertEqual(1, means[3])
+        assert(4 == len(means))
+        assert(1 == sample_rate)
+        assert(0.25 == means[0])
+        assert(.5 == means[1])
+        assert(.75 == means[2])
+        assert(1 == means[3])
     def test_calculate_means_float(self):
         sample_rate = 16000
         samples = [1] * sample_rate
@@ -92,8 +89,8 @@ class TestRosa:
         samples = samples + [3] * sample_rate
         samples = numpy.array(samples)
         means, sample_rate = rosa.CalculateMeans((samples, sample_rate), segment_duration = 0.01, normalize = False)
-        self.assertEqual(300, len(means))
-        self.assertEqual(100, sample_rate)
-        self.assertTrue(numpy.array_equal([1]*100, means[0:100]))
-        self.assertTrue(numpy.array_equal([2]*100, means[100:200]))
-        self.assertTrue(numpy.array_equal([3]*100, means[200:300]))
+        assert(300 == len(means))
+        assert(100 == sample_rate)
+        assert(numpy.array_equal([1]*100, means[0:100]))
+        assert(numpy.array_equal([2]*100, means[100:200]))
+        assert(numpy.array_equal([3]*100, means[200:300]))
