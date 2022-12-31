2c9dd0fcabeca7b7e4d12969ff7cf0a601137ec7
Marcin Matula <marcin.matula@gmail.com>
2022-12-31 15:53:31 +0100

    config - config_loading_time stored in global data
    config_handler.py - move public functions to a top of module

--- utils/config_common.py
+++ utils/config_common.py
@@ -11,17 +11,18 @@ def get_global_schema_path():
     path = pathlib.Path(__file__).parent.parent.resolve()
     return os_proxy.join(path, "schemas/config.schema.json")
 
-def update_with_default_format(custom_format):
+def update_with_default_format(custom_format, **kwargs):
     if custom_format is None:
         custom_format = {}
     default_format = {}
-    import datetime
-    date = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    if not "config_loading_time" in kwargs:
+        raise Exception("Lack of config_loading_time in kwargs")
+    date = kwargs["config_loading_time"]
     default_format["config_loading_time"] = date
     custom_format.update(default_format)
     return custom_format
 
-def prepare_format_dict(data, custom_format = None):
+def prepare_format_dict(data, custom_format = None, **kwargs):
     def make_dict(data_format):
         _dict = {}
         for kv in data_format:
@@ -32,14 +33,14 @@ def prepare_format_dict(data, custom_format = None):
         format_dict.update(data["format"])
     if custom_format:
         format_dict.update(custom_format)
-    format_dict = update_with_default_format(format_dict)
+    format_dict = update_with_default_format(format_dict, **kwargs)
     return format_dict
 
-def process_format(config_dict, format_dict):
-    format_dict = prepare_format_dict(config_dict, format_dict)
+def process_format(config_dict, format_dict, **kwargs):
+    format_dict = prepare_format_dict(config_dict, format_dict, **kwargs)
     for k,v in config_dict.items():
         if isinstance(v, dict):
-            process_format(v, format_dict)
+            process_format(v, format_dict, **kwargs)
         elif isinstance(v, str):
             try:
                 v = v.format(**format_dict)
--- utils/config_handler.py
+++ utils/config_handler.py
@@ -5,139 +5,21 @@ __license__ = "Apache License"
 __version__ = "2.0"
 __maintainer__ = "Marcin Matula"
 
-from deprecation import deprecated
-
-_global_config_pathes = None
-
 class NoConfigException(Exception):
     pass
 
 class NoAttrConfigException(Exception):
     pass
 
-class _Global:
-    def __init__(self):
-        self.config_pathes = None
-        self.custom_format = None
-    def exist(self):
-        return self.config_pathes != None
-
-__global_config = _Global()
-
-class Config:
-    class _Wrapper:
-        def __init__(self, attr):
-            self.attr = attr
-            if isinstance(self.attr, dict):
-                keys = [k for k in self.attr.keys()]
-                for key in keys:
-                    if isinstance(key, str):
-                        key_splitted = key.split(".")
-                        if len(key_splitted) > 1:
-                            value = self.attr[key]
-                            _attr = self.attr
-                            while len(key_splitted) > 1:
-                                k_ = key_splitted.pop(0)
-                                v_ = key_splitted[0]
-                                if k_ not in _attr:
-                                    d = {v_ : {}}
-                                    _attr[k_] = d
-                                else:
-                                    _attr[k_][v_] = {}
-                                _attr = _attr[k_]
-                            _attr[key_splitted.pop(0)] = value
-        def __getattr__(self, attr):
-            if isinstance(self.attr, dict):
-                if attr in self.attr:
-                    if isinstance(self.attr[attr], dict):
-                        return Config._Wrapper(self.attr[attr])
-                    elif isinstance(self.attr[attr], list):
-                        wrappers = []
-                        for a in self.attr[attr]:
-                            if isinstance(a, list) or isinstance(a, dict):
-                                wrappers.append(Config._Wrapper(a))
-                            else:
-                                wrappers.append(a)
-                        return wrappers
-                    else:
-                        return self.attr[attr]
-                else:
-                    raise AttributeError(f"Attribute error: {attr}")
-        def __getitem__(self, k):
-            array = k.split(".")
-            attr = self.attr
-            for a in array:
-                attr = attr[a]
-            return attr
-    def make_format(self, data, custom_format = None):
-        from vatf.utils import config_common
-        return config_common.process_format(data, custom_format)
-    def __init__(self, data, custom_format = None):
-        if isinstance(data, Config):
-            data = data.get_raw_data_copy()
-        import copy
-        self.__data = copy.deepcopy(data)
-        self.data = copy.deepcopy(data)
-        self.data = self.make_format(self.data, custom_format)
-    def get_raw_data(self):
-        return self.__data
-    def get_raw_data_copy(self):
-        import copy
-        return copy.deepcopy(self.get_raw_data())
-    def __getattr__(self, attr):
-        return getattr(Config._Wrapper(self.data), attr)
-    def __getitem__(self, item):
-        _data = Config._Wrapper(self.data)
-        if isinstance(item, str):
-            item = item.split(".")
-            if len(item) == 1:
-                return _data[item[0]]
-            else:
-                data = _data
-                for i in item:
-                    data = data[i]
-                return data
-    def get(self, item, raiseIfNotFound = True):
-        try:
-            return self[item]
-        except KeyError as ke:
-            return None
-
 def init_configs(config_pathes, custom_format = None):
-    global __global_config
-    __global_config.config_pathes = config_pathes
-    __global_config.custom_format = custom_format
+    reset_configs()
+    global _global_data
+    _global_data.config_pathes = config_pathes
+    _global_data.custom_format = custom_format
 
 def reset_configs():
-    global _global_config
-    _global_config = _Global()
-
-def get():
-    return _global_config_pathes
-
-def _handle_global_config(custom_format):
-    global __global_config
-    if not __global_config.exist():
-        raise NoConfigException("no config is existing")
-    from vatf.utils import config_loader
-    format_dict = {}
-    if __global_config.custom_format:
-        format_dict.update(__global_config.custom_format)
-    if custom_format:
-        format_dict.update(custom_format)
-    data = config_loader.load(__global_config.config_pathes)
-    return Config(data, custom_format = format_dict)
-
-def _handle_config(config, custom_format):
-    return Config(config, custom_format)
-
-def _handle_config_path(config_path, custom_format):
-    from vatf.utils import config_loader
-    data = config_loader.load(config_path)
-    return Config(data, custom_format)
-
-def _handle_config_dict(kw_config_dict, custom_format):
-    return Config(kw_config_dict, custom_format)
+    global _global_data
+    _global_data = _GlobalData()
 
 def get_config(custom_format = None, **kwargs):
     is_config_path = "config_path" in kwargs.keys()
@@ -205,3 +87,118 @@ def get_var(config_var, custom_format = None, **kwargs):
         raise Exception("config_var must be singe key")
     output = get_vars([config_var], custom_format, **kwargs)
     return output[config_var]
+
+class _GlobalData:
+    def __init__(self):
+        self.config_pathes = None
+        self.custom_format = None
+        import datetime
+        self.config_loading_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    def exist(self):
+        return self.config_pathes != None
+
+_global_data = _GlobalData()
+
+class Config:
+    class _Wrapper:
+        def __init__(self, attr):
+            self.attr = attr
+            if isinstance(self.attr, dict):
+                keys = [k for k in self.attr.keys()]
+                for key in keys:
+                    if isinstance(key, str):
+                        key_splitted = key.split(".")
+                        if len(key_splitted) > 1:
+                            value = self.attr[key]
+                            _attr = self.attr
+                            while len(key_splitted) > 1:
+                                k_ = key_splitted.pop(0)
+                                v_ = key_splitted[0]
+                                if k_ not in _attr:
+                                    d = {v_ : {}}
+                                    _attr[k_] = d
+                                else:
+                                    _attr[k_][v_] = {}
+                                _attr = _attr[k_]
+                            _attr[key_splitted.pop(0)] = value
+        def __getattr__(self, attr):
+            if isinstance(self.attr, dict):
+                if attr in self.attr:
+                    if isinstance(self.attr[attr], dict):
+                        return Config._Wrapper(self.attr[attr])
+                    elif isinstance(self.attr[attr], list):
+                        wrappers = []
+                        for a in self.attr[attr]:
+                            if isinstance(a, list) or isinstance(a, dict):
+                                wrappers.append(Config._Wrapper(a))
+                            else:
+                                wrappers.append(a)
+                        return wrappers
+                    else:
+                        return self.attr[attr]
+                else:
+                    raise AttributeError(f"Attribute error: {attr}")
+        def __getitem__(self, k):
+            array = k.split(".")
+            attr = self.attr
+            for a in array:
+                attr = attr[a]
+            return attr
+    def make_format(self, data, custom_format = None):
+        from vatf.utils import config_common
+        return config_common.process_format(data, custom_format, config_loading_time = _global_data.config_loading_time)
+    def __init__(self, data, custom_format = None):
+        if isinstance(data, Config):
+            data = data.get_raw_data_copy()
+        import copy
+        self.__data = copy.deepcopy(data)
+        self.data = copy.deepcopy(data)
+        self.data = self.make_format(self.data, custom_format)
+    def get_raw_data(self):
+        return self.__data
+    def get_raw_data_copy(self):
+        import copy
+        return copy.deepcopy(self.get_raw_data())
+    def __getattr__(self, attr):
+        return getattr(Config._Wrapper(self.data), attr)
+    def __getitem__(self, item):
+        _data = Config._Wrapper(self.data)
+        if isinstance(item, str):
+            item = item.split(".")
+            if len(item) == 1:
+                return _data[item[0]]
+            else:
+                data = _data
+                for i in item:
+                    data = data[i]
+                return data
+    def get(self, item, raiseIfNotFound = True):
+        try:
+            return self[item]
+        except KeyError as ke:
+            return None
+
+
+def _handle_global_config(custom_format):
+    global _global_data
+    if not _global_data.exist():
+        raise NoConfigException("no config is existing")
+    from vatf.utils import config_loader
+    format_dict = {}
+    if _global_data.custom_format:
+        format_dict.update(_global_data.custom_format)
+    if custom_format:
+        format_dict.update(custom_format)
+    data = config_loader.load(_global_data.config_pathes)
+    return Config(data, custom_format = format_dict)
+
+def _handle_config(config, custom_format):
+    return Config(config, custom_format)
+
+def _handle_config_path(config_path, custom_format):
+    from vatf.utils import config_loader
+    data = config_loader.load(config_path)
+    return Config(data, custom_format)
+
+def _handle_config_dict(kw_config_dict, custom_format):
+    return Config(kw_config_dict, custom_format)
