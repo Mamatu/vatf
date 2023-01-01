79b575b32e10a1baa369f195050d5f90087fe05f
Marcin Matula <marcin.matula@gmail.com>
2023-01-01 23:05:41 +0100

    loop.py - module

---
+++ utils/loop.py
@@ -0,0 +1,11 @@
+def wait_until(callback, pause, timeout):
+    import time
+    start_point = time.time()
+    while True:
+        status = callback()
+        if status: return True
+        time.sleep(pause)
+        end_point = time.time()
+        if (end_point - start_point) > timeout:
+            print(f"timeout: {end_point} {start_point}")
+            return False
