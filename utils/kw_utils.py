f319ba80c7c90d8e07f96b49ae9c3a4bc8c6d5a2
Marcin Matula <marcin.matula@gmail.com>
2023-01-02 21:09:38 +0100

    kw_utils.py - removed prints

--- utils/kw_utils.py
+++ utils/kw_utils.py
@@ -4,13 +4,9 @@ def handle_kwargs(keys, default_output = None, is_required = False, **kwargs):
     keys_in_kwargs = [k for k in keys if k in kwargs]
     if len(keys_in_kwargs) > 1:
         raise Exception(f"Only one alternative {keys_in_kwargs} can be parameter")
-    print(keys_in_kwargs)
     if len(keys_in_kwargs) == 0:
-        print(is_required)
         if is_required:
             raise Exception(f"Arg {keys} is required")
-        print("2")
         return default_output
-    print("1")
     return kwargs[keys_in_kwargs[0]]
 
