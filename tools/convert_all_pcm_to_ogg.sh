da826d0991c2b574b8a9aa5eac44b15fd171ebdb
Marcin Matula <marcin.matula@gmail.com>
2023-01-08 21:59:28 +0100

    convert_all_pcm_to_ogg.sh - removed trash

--- tools/convert_all_pcm_to_ogg.sh
+++ tools/convert_all_pcm_to_ogg.sh
@@ -1,4 +1,2 @@
 #/bin/bash
-    return f"ffmpeg -f {fformat} -ar {framerate} -ac {channels} -i {input} {output}"
-
 find .. -name \"*.pcm\" | parallel -I % --max-args 1 --jobs 8 ffmpeg -f s16le -ac 1 -ar 44100 % %.ogg
