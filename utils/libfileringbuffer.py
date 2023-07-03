b1587febe7cb0533b9749ffd82beada98e2d9593
Marcin Matula <marcin.matula@gmail.com>
2023-07-03 23:18:27 +0200

    libfileringbuffer - work contiuantion

--- utils/libfileringbuffer.py
+++ utils/libfileringbuffer.py
@@ -6,23 +6,26 @@ class FileRingBuffer:
     """
     Ringbuffer realized by files in the fs
     """
-    def __init__(self, command, fifo_file, ringbuffer_timeout_or_lines, wait_timeout, chunks_dir,  chunks_count = 10):
+    def __init__(self, command, fifo_file, chunk_lines, chunks_count, chunks_dir):
         self.command = command
         self.fifo_file = fifo_file
-        self.ringbuffer_timeout_or_lines = ringbuffer_timeout_or_lines
+        self.chunk_lines = chunk_lines
         self.chunks_count = chunks_count
-        self.chunk_timeout = self.ringbuffer_timeout / self.chunks_count
-        self.wait_timeout = wait_timeout
         self.chunks_dir = chunks_dir
         self.bg_process = None
-        def target(fifo_file, chunks_count, wait_timeout, is_stopped):
+        def target(fifo_file, chunk_lines, chunks_count, wait_timeout, is_stopped):
+            idx = 0
+            get_chunk_name = lambda: f"chunk_{idx}.log"
+            chunk_file = get_chunk_name()
             while not is_stopped():
-                shell.bg(f"cat {fifo_file} > ")
-                pass
-        self.thread = Thread(target = target, args = [self.fifo_file, self.chunks_count, self.wait_timeout])
+                shell.fg(f"cat {fifo_file} >> {chunk_file}")
+                wc_chunk_file = shell.fg(f"wc -l {chunk_file}")
+                if wc_chunk_file > chunk_lines:
+                    idx = idx + 1
+                    chunk_file = get_chunk_name()
+        self.thread = Thread(target = target, args = [self.fifo_file, self.chunk_lines, self.chunks_count, self.wait_timeout])
     def start(self):
         self.bg_process = shell.bg(f"mkfifo {self.fifo_file}; {self.command} > {self.fifo_file}")
-
     def stop(self):
         shell.kill(self.bg_process)
         self.thread.stop()
