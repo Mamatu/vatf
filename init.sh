f699c72fda6d3be1a378e3c9e7217f0114b73b26
Marcin Matula <marcin.matula@gmail.com>
2023-11-08 14:38:58 +0100

    init.sh invoke make_frb.sh. make_frb.sh is main script to build file ring buffer

--- init.sh
+++ init.sh
@@ -1,8 +1,2 @@
-mkdir bin
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../bin ../file_ring_buffer
-make
-make install
-cd -
-rm -r build
+#!/bin/bash
+./make_frb.sh
--- make_frb.sh
+++ make_frb.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-FRB_PROJECT=/tmp/frb
-
 build () {
   VATF_PWD=$PWD
+  FRB_PROJECT=$VATF_PWD/bin
   rm -r $FRB_PROJECT/build | true
   mkdir -p $FRB_PROJECT/build
   cd $FRB_PROJECT/build
-  cmake $VATF_PWD/file_ring_buffer
+  cmake -DCMAKE_INSTALL_PREFIX:PATH=$VATF_PWD/bin $VATF_PWD/file_ring_buffer
   make
+  make install
   cd -
 }
 
