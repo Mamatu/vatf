403aae823a48eb247336f025ec3e85266b214997
Marcin Matula <marcin.matula@gmail.com>
2023-11-14 10:27:22 +0100

    init.sh renamed into build_commands.sh

--- run_uts.sh
+++ run_uts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./init.sh && bin/frb_tests && ./make_dlt.sh && ./python.sh -m pytest $(find -name "ut_*.py" -not -path "./utils/pylibcommons/*")
+./build_commands.sh && bin/frb_tests && ./make_dlt.sh && ./python.sh -m pytest $(find -name "ut_*.py" -not -path "./utils/pylibcommons/*")
