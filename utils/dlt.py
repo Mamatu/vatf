3a07c5e8f18c3d5861cb24b35cb2963794231fd5
Marcin Matula <marcin.matula@gmail.com>
2023-11-14 18:08:46 +0100

    dlt.py - delay changed into 10 ms

--- utils/dlt.py
+++ utils/dlt.py
@@ -8,7 +8,7 @@ class DltWriter:
         self._dlt_rootfs = os.path.join(dlt_project_path, "rootfs")
         self._dlt_example_user_path = os.path.join(self._dlt_rootfs, "bin/dlt-example-user")
     def _write(self, payload, log_level = 2, count = 1):
-        return f"LD_LIBRARY_PATH={self._dlt_rootfs}/lib {self._dlt_example_user_path} -l {log_level} -n {count} -d 0 \"{payload}\""
+        return f"LD_LIBRARY_PATH={self._dlt_rootfs}/lib {self._dlt_example_user_path} -l {log_level} -n {count} -d 10 \"{payload}\""
     def write(self, payload, log_level = 2, count = 1):
         shell.fg(self._write(payload = payload, log_level = log_level, count = count))
     def write_in_async_loop(self, pre_callback):
