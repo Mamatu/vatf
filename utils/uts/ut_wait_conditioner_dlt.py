a03b82b9b91ea5d9dfe21aae105e34d43b648d0f
Marcin Matula <marcin.matula@gmail.com>
2023-11-15 15:40:10 +0100

    ut_wait_conditioner_dlt.py - refactoring with decorator

--- utils/uts/ut_wait_conditioner_dlt.py
+++ utils/uts/ut_wait_conditioner_dlt.py
@@ -19,6 +19,8 @@ from vatf.utils import os_proxy
 
 _written_lines_count = 0
 
+import time
+
 def generate_line():
     global _written_lines_count
     now = datetime.datetime.now()
@@ -73,7 +75,8 @@ def _test_wrapper(test_func):
             assert False
         finally:
             writer.stop()
-            tempdir.cleanup()
+            #tempdir.cleanup()
+        time.sleep(200)
     return inner
 
 @_test_wrapper
@@ -102,6 +105,57 @@ def test_libcmdringbuffer_1(writer, tempdir):
     assert wait.wait_for_regex("line_10", timeout = 20, config = config)
     wait.stop()
 
+@_test_wrapper
+def test_libcmdringbuffer_1(writer, tempdir):
+    from vatf.utils import lib_log_snapshot
+    from vatf.utils import os_proxy
+    writer_t = None
+    chunks_dir = os.path.join(tempdir.name, "chunks")
+    try:
+        import shutil
+        shutil.rmtree(chunks_dir)
+    except FileNotFoundError:
+        pass
+    config = {
+        "wait_for_regex.command" : f"{get_receive_path()} -a 127.0.0.1",
+        "wait_for_regex.is_file_ring_buffer" : True,
+        "wait_for_regex.lines_count" : 2,
+        "wait_for_regex.chunks_count" : 2,
+        "wait_for_regex.workspace" : f"{tempdir.name}",
+        "wait_for_regex.date_format" : "%Y-%m-%d %H:%M:%S.%f",
+        "wait_for_regex.date_regex" : "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]"
+    }
+    wait.start(config = config)
+    writer_t = writer.write_in_async_loop(pre_callback = generate_line)
+    chunks_dir_1 = os.listdir(os.path.join(tempdir.name, "chunks"))
+    assert wait.wait_for_regex("line_3", timeout = 20, config = config)
+    wait.stop()
+
+@_test_wrapper
+def test_libcmdringbuffer_2(writer, tempdir):
+    from vatf.utils import lib_log_snapshot
+    from vatf.utils import os_proxy
+    writer_t = None
+    chunks_dir = os.path.join(tempdir.name, "chunks")
+    try:
+        import shutil
+        shutil.rmtree(chunks_dir)
+    except FileNotFoundError:
+        pass
+    config = {
+        "wait_for_regex.command" : f"{get_receive_path()} -a 127.0.0.1",
+        "wait_for_regex.is_file_ring_buffer" : True,
+        "wait_for_regex.lines_count" : 2,
+        "wait_for_regex.chunks_count" : 2,
+        "wait_for_regex.workspace" : f"{tempdir.name}",
+        "wait_for_regex.date_format" : "%Y-%m-%d %H:%M:%S.%f",
+        "wait_for_regex.date_regex" : "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9][0-9][0-9]"
+    }
+    wait.start(config = config)
+    writer.write_in_async_loop(pre_callback = generate_line)
+    chunks_dir_1 = os.listdir(os.path.join(tempdir.name, "chunks"))
+    assert wait.wait_for_regex("line_3", timeout = 20, config = config)
+    wait.stop()
 
 #def test_libcmdringbuffer():
 #    with mocked_now(datetime.datetime(2022, 1, 29, hour = 20, minute = 54, second = 54, microsecond = 000000)):
