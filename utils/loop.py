15fa7620a60395260ae18e498a10b204e7857465
Marcin Matula <marcin.matula@gmail.com>
2023-11-27 23:59:49 +0100

    loop.py - support for timeout -1

--- utils/loop.py
+++ utils/loop.py
@@ -1,11 +1,12 @@
+import time
 def wait_until_true(callback, pause, timeout):
-    import time
-    start_point = time.time()
+    if timeout > 0:
+        start_point = time.time()
     while True:
         status = callback()
         if status: return True
         time.sleep(pause)
-        end_point = time.time()
-        if (end_point - start_point) > timeout:
-            print(f"timeout: {end_point} {start_point}")
-            return False
+        if timeout > 0:
+            end_point = time.time()
+            if (end_point - start_point) > timeout:
+                return False
