74a1f8c3a9b5264c9ea398c7fa55c1ec55ec0a0e
Marcin Matula <marcin.matula@gmail.com>
2023-11-29 14:00:13 +0100

    run_uts.sh - Debug mode for frb_tests

--- run_uts.sh
+++ run_uts.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build_commands.sh && bin/frb_tests && ./build_dlt.sh && ./python.sh -m pytest $(find -name "ut_*.py" -not -path "./utils/pylibcommons/*")
+./build_commands.sh && bin/frb_tests Debug && ./build_dlt.sh && ./python.sh -m pytest $(find -name "ut_*.py" -not -path "./utils/pylibcommons/*")
