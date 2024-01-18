6c858c9cd1202c265b005234d6671521178da5e0
Marcin Matula <marcin.matula@gmail.com>
2024-01-18 16:34:22 +0100

    libtools.py - draft of tools feature

--- utils/libtools.py
+++ utils/libtools.py
@@ -18,7 +18,10 @@ def create_tools_dir_form_config(**kwargs):
     tools_path = os.path.join(tools_path, "tools")
     files = [os.path.join(tools_path, f) for f in os.listdir(tools_path)]
     for path in pathes:
-        os.makedirs(path)
-        templates_path = os.path.join(path, "templates")
+        if not os.path.exists(path):
+            os.makedirs(path, exist_ok = True)
+        templates_path = os.path.join(path, "tools/.templates")
+        if not os.path.exists(templates_path):
+            os.makedirs(templates_path, exist_ok = True)
         for file in files:
             shutil.copy(file, templates_path)
