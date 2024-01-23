694e2fe3ae4c6a96c8537bbf61a2a32096071c3b
Marcin Matula <marcin.matula@gmail.com>
2024-01-23 13:39:30 +0100

    libtools.py - script creator code was moved outsibe libtools inside python script in tools directory

--- utils/libtools.py
+++ utils/libtools.py
@@ -4,6 +4,9 @@ import shutil
 
 from vatf.utils import config_handler
 
+import pathlib
+import importlib
+
 def create_tools_dir_form_config(**kwargs):
     tools_pathes_key = "tools.pathes"
     config = config_handler.get_config(**kwargs)
@@ -24,13 +27,12 @@ def create_tools_dir_form_config(**kwargs):
         if not os.path.exists(templates_path):
             os.makedirs(templates_path, exist_ok = True)
         for file in files:
-            dir_file = os.path.join(tools_path, file)
-            shutil.copy(dir_file, templates_path)
-            if file in _scripts.keys():
-                _scripts[file](path)
-
-def _create_convert_all_pcm_to_ogg(path):
-    with open(os.path.join(path, "tools/convert_all_pcm_to_ogg.sh"), "w") as f:
-        f.write("WORKSPACE_PATH=.. bash ./.templates/template.convert_all_pcm_to_ogg.sh")
-
-_scripts = {"template.convert_all_pcm_to_ogg.sh" : _create_convert_all_pcm_to_ogg}
+            if file.endswith(".py"):
+                file = file[:-3]
+                module = importlib.import_module(f"vatf.tools.{file}")
+                script = module.create_script()
+                with open(os.path.join(path, "tools", module.get_script_name()), "w") as f:
+                    f.write(script)
+            if file.endswith(".sh"):
+                dir_file = os.path.join(tools_path, file)
+                shutil.copy(dir_file, templates_path)
