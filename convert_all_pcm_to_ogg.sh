96b28e8e7e04663f2ec0b1ef0e102fca8772daa1
Marcin Matula <marcin.matula@gmail.com>
2024-01-23 13:39:30 +0100

    libtools.py - script creator code was moved outsibe libtools inside python script in tools directory

---
+++ convert_all_pcm_to_ogg.sh
@@ -0,0 +1,1 @@
+WORKSPACE_PATH=.. bash ./.templates/template.convert_all_pcm_to_ogg.sh
---
+++ tools/convert_all_pcm_to_ogg.py
@@ -0,0 +1,5 @@
+def create_script():
+    return "WORKSPACE_PATH=.. bash ./.templates/template.convert_all_pcm_to_ogg.sh"
+
+def get_script_name():
+    return "convert_all_pcm_to_ogg.sh"
