c07852e8bec09ec0f209b665f53af3f58d882387
Marcin Matula <marcin.matula@partner.bmw.de>
2024-01-27 22:00:46 +0100

    tools/template.convert_all_pcm_to_ogg.sh - using -i param to define the input

--- tools/template.convert_all_pcm_to_ogg.sh
+++ tools/template.convert_all_pcm_to_ogg.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-find ${WORKSPACE_PATH} -name "*.pcm" | parallel -I % --max-args 1 --jobs 8 ffmpeg -f s16le -ac 1 -ar 44100 % %.ogg
+find ${WORKSPACE_PATH} -name "*.pcm" | parallel -I % --max-args 1 --jobs 8 ffmpeg -f s16le -ac 1 -ar 44100 -i % %.ogg
