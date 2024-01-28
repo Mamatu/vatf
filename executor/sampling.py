8ff48bcc0b4d1896dabb78567bbecb40db170f3f
Marcin Matula <marcin.matula@partner.bmw.de>
2024-01-28 17:35:31 +0100

    sampling.py - adapt to current state of framework

--- executor/sampling.py
+++ executor/sampling.py
@@ -40,8 +40,8 @@ def _convert_pcm_to_ogg(recording_path, audioConfig):
     vatf.utils.ffmpeg.convert(recording_path, output_path, audioConfig)
     return output_path
 
-def get_creation_date(path, **kwargs):
-    timestamp_format = _get_timestamp_format_from_kwargs(**kwargs)
+def get_creation_date(path, timestamp_format, **kwargs):
+    #timestamp_format = _get_timestamp_format_from_kwargs(**kwargs)
     if not os.path.exists(path):
         raise FileNotFoundError(path)
     read_date_stat = lambda: utils.get_modification_date(path)
@@ -84,8 +84,8 @@ def extract_sample(start_regex_timestamp, end_regex_timestamp, recording_start_t
     logging.info(f"Write sample to {sample_path}")
     return sample_path
 
-def extract_samples(recording_start_timestamp, regexes, recording_path, samples_path, audioConfig = vatf.utils.ac.AudioConfig(vatf.utils.ac.Format.s16le, channels = 1, framerate = 44100), sample_format = "ogg", **kwargs):
-    timestamp_format = _get_timestamp_format_from_kwargs(**kwargs)
+def extract_samples(recording_start_timestamp, regexes, recording_path, samples_path, timestamp_format, audioConfig = vatf.utils.ac.AudioConfig(vatf.utils.ac.Format.s16le, channels = 1, framerate = 44100), sample_format = "ogg", **kwargs):
+    #timestamp_format = _get_timestamp_format_from_kwargs(**kwargs)
     import librosa
     audio = []
     sr = None
@@ -112,8 +112,8 @@ def extract_samples(recording_start_timestamp, regexes, recording_path, samples_
     logging.info(f"Extracted {len(sample_paths)} samples. {not_matched_samples_count} samples could not extract (not in recording)")
     return sample_paths
 
-def find_start_end_regexes(path_to_log, start_regex, end_regex, from_line, max_count, **kwargs):
-    timestamp_regex = _get_timestamp_regex_from_kwargs(**kwargs)
+def find_start_end_regexes(path_to_log, start_regex, end_regex, from_line, max_count, timestamp_regex, **kwargs):
+    #timestamp_regex = _get_timestamp_regex_from_kwargs(**kwargs)
     start_regexes = utils.grep_regex_in_line(filepath = path_to_log, grep_regex = start_regex, match_regex = timestamp_regex, maxCount = max_count)
     end_regexes = utils.grep_regex_in_line(filepath = path_to_log, grep_regex = end_regex, match_regex = timestamp_regex, maxCount = max_count)
     regexes = [(start_regexes[i], end_regexes[i]) for i in range(0, len(start_regexes))]
@@ -130,38 +130,43 @@ def _checks_args(args):
     def not_mandatory(arg, path):
         if path and not os.path.exists(path):
             raise Exception(f"Not existed {arg}: {path}")
-    mandatory("args.path_to_log", args.path_to_log)
-    mandatory("args.path_to_recording", args.path_to_recording)
-    mandatory("args.path_to_samples", args.path_to_samples)
+    def mandatory_create_if_not_exist(arg, path):
+        if not os.path.exists(path):
+            os.makedirs(path)
+    mandatory("{args.path_to_log=}", args.path_to_log)
+    mandatory("{args.path_to_recording=}", args.path_to_recording)
+    mandatory_create_if_not_exist("{args.path_to_samples=}", args.path_to_samples)
     not_mandatory("{args.path_to_recording_date=}", args.path_to_recording_date)
 
 def main(args):
     _checks_args(args)
-    start_date = get_recording_start_date(args.path_to_recording, args.path_to_recording_date)
-    start_date = config.ConvertToLogZone(start_date)
-    if not args.start_regex and not args.end_regex:
-        regexes = config.GetRegexesForSampling()
-        args.start_regex = regexes[0][0]
-        args.end_regex = regexes[0][1]
-        if len(regexes) > 1:
-            logging.error("Number of regexes > 1 is not supported. Only the first one will be used")
-    elif not (args.start_regex and args.end_regex):
-        raise Exception("Inproper pair of start and end regex. Please provide both as arguments or in config")
+    start_date = get_recording_start_date(args.path_to_recording, args.path_to_recording_date, args.timestamp_format)
+    #start_date = config.ConvertToLogZone(start_date)
+    #if not args.regexes.begin and not args.end_regex:
+    #    regexes = config.GetRegexesForSampling()
+    #    args.start_regex = regexes[0][0]
+    #    args.end_regex = regexes[0][1]
+    #    if len(regexes) > 1:
+    #        logging.error("Number of regexes > 1 is not supported. Only the first one will be used")
+    if not (args.regexes.begin and args.regexes.end):
+        raise Exception("Inproper pair of start and end regex. Please provide both as arguments in config")
     maxCount = -1
-    if args.count:
-        maxCount = args.count
-    regexes = find_start_end_regexes(args.path_to_log, args.start_regex, args.end_regex, args.from_line, maxCount)
-    if args.path_to_search_lines:
-        with open(args.lines_save_to, "w") as f:
-            array = []
-            for start_end in regexes:
-                array.append(start_end[0].line_number)
-                array.append(start_end[1].line_number)
-            f.write("\n".join(array))
-    sample_pathes = extract_samples(start_date, regexes, args.path_to_recording, args.path_to_samples)
+    #if args.count:
+    #    maxCount = args.count
+    #regexes = find_start_end_regexes(args.path_to_log, args.regexes.begin, args.regexes.end, args.from_line, maxCount)
+    regexes = find_start_end_regexes(args.path_to_log, args.regexes.begin, args.regexes.end, 0, maxCount, args.timestamp_regex)
+    #if args.path_to_search_lines:
+    #if args.path_to_log:
+    #    with open(args.lines_save_to, "w") as f:
+    #        array = []
+    #        for start_end in regexes:
+    #            array.append(start_end[0].line_number)
+    #            array.append(start_end[1].line_number)
+    #        f.write("\n".join(array))
+    sample_pathes = extract_samples(start_date, regexes, args.path_to_recording, args.path_to_samples, args.timestamp_format)
     logging.info(f"Created: {sample_pathes}")
 
 def start_from_config(**kwargs):
-    from vatf.utils import config
+    from vatf.utils import config_handler
     config = config_handler.get_config(**kwargs)
     main(config.sampling)
