28a57b36b528beb68a29c7ecee4e2c13a5ed942e
Marcin Matula <marcin.matula@partner.bmw.de>
2024-03-13 13:16:46 +0100

    create_report_of_the_last_cycle impl

---
+++ tools/template.create_report_of_the_last_cycle.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+find ${WORKSPACE_PATH} -name status -type f | sed "s|./\(test_.*\)\/session.*|\1|g" | uniq -c | awk '{print "(" $2 ", number of sessions: " $1 ")";system("cat " $2"/$(ls " $2 "| tail -n +" $1 ")/status/status")}'
