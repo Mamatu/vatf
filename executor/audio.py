aade7f56f04291ee0704b62cc5c5cf6dbd05ed98
Marcin Matula <marcin.matula@partner.bmw.de>
2024-03-20 14:47:59 +0100

    utils/gstreamer.py - hardcoded DATE_FORMAT replaced by date_format read from config

--- executor/audio.py
+++ executor/audio.py
@@ -11,16 +11,7 @@ from vatf import vatf_api
 from vatf.utils import config_handler, utils
 import logging
 
-
 _processes = []
-
-@vatf_api.public_api("audio")
-def record_inputs_outputs():
-    global _processes
-    output_path = mkdir.mkdir_with_counter("./recordings/session")
-    rec_command = f"python3 vatf/utils/papy.py --recorder=gst --rec --dir {output_path}"
-    logging.debug(f"{record_inputs_outputs.__name__}: {rec_command}")
-    _processes.append(shell.bg(rec_command))
 
 @vatf_api.public_api("audio")
 def record_inputs_outputs_from_config(**kwargs):
@@ -28,9 +19,11 @@ def record_inputs_outputs_from_config(**kwargs):
     audio_dir_path_key = "audio.path"
     config = config_handler.get_config(**kwargs)
     output_path = config.audio.path
+    date_format = config.audio.date_format
+    date_linux_date_format = config.audio.date_linux_date_format
     print(f"output_path: {output_path}")
     mkdir.mkdir(output_path)
-    rec_command = f"python3 vatf/utils/papy.py --recorder=gst --rec --dir {output_path}"
+    rec_command = f"python3 vatf/utils/papy.py --recorder=gst --rec --dir {output_path} --date_format='{date_format}' --date_linux_date_format='{date_linux_date_format}'"
     logging.debug(f"{record_inputs_outputs_from_config.__name__}: {rec_command}")
     _processes.append(shell.bg(rec_command))
 
